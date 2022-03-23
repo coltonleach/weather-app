7e1863ed37f3022936d553a2369f7a5cc4c0d3bd
Colton Leach <leach.colton@hotmail.com>
2022-03-23 01:07:15 -0500

    all of the information needed is finished, next step is to add the correct image for the type of weather, then implement the themes

--- src/App.js
+++ src/App.js
@@ -39,20 +39,41 @@ const App = () => {
     })
   }, [])
   
-  return (
+  return (<>
+    <p className="location-name">{location}</p>
     <div className='container'>
-      <p className="location-name">{location}</p>
       <SearchBar location={location} searchLocation={searchLocation} handleSearchChange={handleSearchChange}/>
       {loading
       ? <div>aaa</div>
       : <TodayCard current={current} todayInfo={forecast[0]}/>}
       
-      <div className="small-card-container">
-        <SmallCard />
-        <SmallCard />
-        <SmallCard />
-      </div>
+      {loading
+      ? <div>bbb</div>
+      : <div className="small-card-container">
+          {forecast.map(day => {
+            return <SmallCard forecast={day}/>
+          })}
+        </div>
+      }
+      {
+      loading
+      ? null
+      : current.is_day
+        ? <svg id="sun" width="868" height="901" viewBox="0 0 868 901" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="368" cy="401" r="500" fill="#FFED90" fill-opacity="0.1"/>
+            <circle cx="368" cy="401" r="369.863" fill="#FFED90" fill-opacity="0.15"/>
+            <circle cx="368" cy="401" r="260.274" fill="#FFED90" fill-opacity="0.45"/>
+            <circle cx="368" cy="401" r="158.904" fill="#FFED91" fill-opacity="0.6"/>
+          </svg>
+        : <svg id="moon" width="840" height="840" viewBox="0 0 840 840" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="420" cy="420" r="420" fill="#F1F0FF" fill-opacity="0.06"/>
+            <circle cx="420" cy="420" r="310.685" fill="#F1F0FF" fill-opacity="0.15"/>
+            <circle cx="420" cy="420" r="218.63" fill="#F1F0FF" fill-opacity="0.35"/>
+            <circle cx="420" cy="420" r="133.479" fill="#F1F0FF" fill-opacity="0.65"/>
+          </svg>
+      }
     </div>
+    </>
   )
 }
 
